7bf79f844dc9d5b60628539ec74055d03e8ec5bf
Nikita Stepanov <stepanovnickita1994@gmail.com>
2024-01-27 11:22:59 -0600

    correct script

--- build-and-run.sh
+++ build-and-run.sh
@@ -6,8 +6,10 @@ IMAGE_NAME="webhook-handler-app-image"
 
 # Stop the running container (if it exists)
 if docker ps -a --filter "name=$APP_NAME" --format '{{.Names}}' | grep -q $APP_NAME; then
+  echo "Stopping and removing the running container..."
   docker stop $APP_NAME
   docker rm $APP_NAME
+  echo "Stopping and removing the running container... - Done."
 fi
 
 # Build the Docker image
@@ -16,7 +18,7 @@ docker build -t $IMAGE_NAME .
 # Run the Docker container
 docker run -d -p 3000:3000 --name $APP_NAME $IMAGE_NAME
 
-# Remove all unused images related to the app
-docker rmi "$(docker images -q -f "dangling=true" -f "ancestor=$IMAGE_NAME")"
+# Remove all unused images and data
+docker system prune -af
 
 echo "Docker image built, app is running, and unused images are removed."
