fde7064d93b0ad5f4b59764909cb487d122de26c
Nikita Stepanov <stepanovnickita1994@gmail.com>
2024-01-27 10:57:32 -0600

    initial

---
+++ webhook-handler.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const { spawn } = require('child_process');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.WEBHOOK_HANDLER_PORT || 3000;
+
+// Parse JSON in request body
+app.use(bodyParser.json());
+
+// Define a POST route to handle webhook requests
+app.post('/webhook/expenses-bot', (req, res) => {
+	// Parse the webhook payload
+	// const { repository, ref, action } = req.body;
+	console.log('Received webhook request');
+	console.log(req.body);
+
+	// Perform any validation or filtering based on the payload
+
+	// Execute the shell script
+	const shellScript = spawn('sh', ['/home/crearec/test.sh']);
+
+	shellScript.stdout.on('data', (data) => {
+		console.log(`stdout: ${data}`);
+	});
+
+	shellScript.stderr.on('data', (data) => {
+		console.error(`stderr: ${data}`);
+	});
+
+	shellScript.on('close', (code) => {
+		console.log(`Shell script exited with code ${code}`);
+		res.status(200).send('Webhook received and processed.');
+	});
+});
+
+// Start the server
+app.listen(port, () => {
+	console.log(`Server is running on port ${port}`);
+});
